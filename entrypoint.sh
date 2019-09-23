b3b2ad219b1e1db2b0366b5cf89c6fc7b123ad6e
Bruno Paz <brunopaz@sapo.pt>
2019-09-23 20:34:35 +0100

    test: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
-#!/bin/sh -l
+#!/bin/sh
+
+set -ex
 
 if [[ ! -z "$EXTRA_PLUGINS" ]]; then
   npm i -g $EXTRA_PLUGINS
 fi
 
+echo $0
+echo $1
+echo $@
+
 # Run semantic release command
-semantic-release "$@"
+semantic-release $@
